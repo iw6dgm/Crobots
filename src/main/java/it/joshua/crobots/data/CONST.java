292c49a406b9aabff8ff36be66f40292599fb645
joshua <joshua@deepnet>
2012-11-23 19:29:52 +0100

    minor changes

--- src/main/java/it/joshua/crobots/data/CONST.java
+++ src/main/java/it/joshua/crobots/data/CONST.java
@@ -14,4 +14,8 @@ public class CONST {
     public static final String DIGEST_ALGO = "MD5";
     public static final String HTTP_SITE_URL = "http://localhost/crobots/sql";
     public static final String CMD_SCRIPT = "crobots.bat";
+    public static final String CMD_PATH = "C:\\crobots";
+    public static final String DATA_FILE = "torneo.dat";
+    public static final String JDBC = "jdbc:mysql://127.0.01:3306/crobots?user=crobots&password=";
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
 }
