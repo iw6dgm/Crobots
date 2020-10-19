52c4546a0132e21dc21bb2879ed7be70dfe98c52
joshua <iw6dgm@tiscali.it>
2020-10-19 11:37:19 +0100

    remove unused import

--- pairings/src/main/java/Pairings.java
+++ pairings/src/main/java/Pairings.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
